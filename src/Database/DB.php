98f7132a94fe918cbe2022eb36d7396dd492d89b
NemethRajmondAdam <152845824+NemethRajmondAdam@users.noreply.github.com>
2024-09-16 09:36:09 +0200

    asd

---
+++ src/Database/DB.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Database;
+
+class DB
+{
+
+    const HOST = 'localhost';
+    const USER = 'root';
+    const PASSWORD = null;
+    const DATABASE = 'postoffice';
+
+    protected $mysqli;
+    function __construct(
+        $host = self::HOST,
+        $user = self::USER,
+        $password = self::PASSWORD,
+        $database = self::DATABASE)
+    {
+        $this->mysqli = mysqli_connect(
+            hostname: $host,
+            username: $user,
+            password: $password,
+            database: $database);
+
+        if (!$this->mysqli) {
+            die("Connection failed: ". mysqli_connect_error());
+        }
+        $this->mysqli->set_charset("utf8mb4");
+    }
+
+    function __destruct()
+    {
+        $this->mysqli->close();
+    }
+}
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c8b1554597003b9043d0192e4dcf77997b6642d5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c8b1554597003b9043d0192e4dcf77997b6642d5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
